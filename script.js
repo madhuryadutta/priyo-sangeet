3323cacb713fc0f900dd03e743483cf422a8b7eb
madhuryadutta <madhuryadutta@users.noreply.github.com>
2024-03-26 02:48:51 +0000

    Generated the Cover arts & Playlist script

--- script.js
+++ script.js
@@ -21,7 +21,27 @@ let isPlaying = false;
 let index = 0;
 
 const songDataBase = [
-
+{
+                        songSrc:"./music/better-day-186374.mp3",
+                        title: "better-day-186374.mp3",
+                        artist: "better-day-186374.mp3",
+                        imgSrc: "./img/better-day-186374.mp3.jpg",
+                    },{
+                        songSrc:"./music/ethereal-vistas-191254.mp3",
+                        title: "ethereal-vistas-191254.mp3",
+                        artist: "ethereal-vistas-191254.mp3",
+                        imgSrc: "./img/ethereal-vistas-191254.mp3.jpg",
+                    },{
+                        songSrc:"./music/solitude-dark-ambient-electronic-197737.mp3",
+                        title: "solitude-dark-ambient-electronic-197737.mp3",
+                        artist: "solitude-dark-ambient-electronic-197737.mp3",
+                        imgSrc: "./img/solitude-dark-ambient-electronic-197737.mp3.jpg",
+                    },{
+                        songSrc:"./music/tunetank.com_259_world-we-live-in_by_motion-productions.mp3",
+                        title: "tunetank.com_259_world-we-live-in_by_motion-productions.mp3",
+                        artist: "tunetank.com_259_world-we-live-in_by_motion-productions.mp3",
+                        imgSrc: "./img/tunetank.com_259_world-we-live-in_by_motion-productions.mp3.jpg",
+                    },
 ];
 
 const loadMusic = () => {
