20e5c12f477479e17facfaa18cf6fff147300616
madhuryadutta <madhuryadutta@users.noreply.github.com>
2024-03-26 03:54:06 +0000

    Generated the Cover arts & Playlist script

--- src/songs.js
+++ src/songs.js
@@ -1,3 +1,28 @@
-const songs = [];
+const songs = [{
+                            songSrc:"./song/better-day-186374.mp3",
+                            title: "better-day-186374.mp3",
+                            artist: "better-day-186374.mp3",
+                            imgSrc: "",
+                        },{
+                            songSrc:"./song/ethereal-vistas-191254.mp3",
+                            title: "ethereal-vistas-191254.mp3",
+                            artist: "ethereal-vistas-191254.mp3",
+                            imgSrc: "",
+                        },{
+                            songSrc:"./song/perfect-beauty-191271.mp3",
+                            title: "perfect-beauty-191271.mp3",
+                            artist: "perfect-beauty-191271.mp3",
+                            imgSrc: "",
+                        },{
+                            songSrc:"./song/solitude-dark-ambient-electronic-197737.mp3",
+                            title: "solitude-dark-ambient-electronic-197737.mp3",
+                            artist: "solitude-dark-ambient-electronic-197737.mp3",
+                            imgSrc: "",
+                        },{
+                            songSrc:"./song/tunetank.com_259_world-we-live-in_by_motion-productions.mp3",
+                            title: "tunetank.com_259_world-we-live-in_by_motion-productions.mp3",
+                            artist: "tunetank.com_259_world-we-live-in_by_motion-productions.mp3",
+                            imgSrc: "",
+                        },];
 
 export default songs;
